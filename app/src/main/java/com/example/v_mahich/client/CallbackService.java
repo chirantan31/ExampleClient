7209ba9907e152887fbaaafd90f466230e5d587c
chirantan31 <chirantan31@gmail.com>
2017-11-28 16:22:06 +0530

    Enforcing Google Java Styling

--- app/src/main/java/com/example/v_mahich/client/CallbackService.java
+++ app/src/main/java/com/example/v_mahich/client/CallbackService.java
@@ -9,43 +9,45 @@ import android.util.Log;
 
 /* This Service is called when a download has been completed by the Sneakernet Platform */
 public class CallbackService extends IntentService {
-    public static final String TAG = "CallbackService";
 
-    public CallbackService() {
-        super("CallbackService");
+  public static final String TAG = "CallbackService";
+
+  public CallbackService() {
+    super("CallbackService");
+  }
+
+  /**
+   * The Client App can extract details about the files downloaded and issue its database
+   * and/or notify the user accordingly
+   */
+  @Override
+  protected void onHandleIntent(Intent intent) {
+    if (intent != null) {
+      Log.i(TAG, "onHandleIntent");
+    }
+    // Bundle with details about the files downloaded
+    if (intent.getExtras() != null) {
+      Bundle bundle = intent.getExtras();
     }
 
-    /* The Client App can extract details about the files downloaded and issue its database
-    * and/or notify the user accordingly
-    * */
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            Log.i(TAG , "onHandleIntent");
-        }
-        // Bundle with details about the files downloaded
-        if (intent.getExtras() != null) {
-            Bundle bundle = intent.getExtras();
-        }
+    // TODO: Update database to reflect the newly downloaded files
 
-        // TODO: Update database to reflect the newly downloaded files
+    // Create a notification to alert the user of the files downloaded
+    Notification.Builder mBuilder =
+        new Notification.Builder(this)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Download complete!")
+            .setContentText("Click here to access new files!");
 
-        // Create a notification to alert the user of the files downloaded
-        Notification.Builder mBuilder =
-                new Notification.Builder(this)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("Download complete!")
-                        .setContentText("Click here to access new files!");
+    // Sets an ID for the notification
+    int mNotificationId = 1;
+    // Gets an instance of the NotificationManager service
+    NotificationManager mNotifyMgr =
+        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    // Builds the notification and issues it.
+    mNotifyMgr.notify(mNotificationId, mBuilder.build());
 
-        // Sets an ID for the notification
-        int mNotificationId = 1;
-        // Gets an instance of the NotificationManager service
-        NotificationManager mNotifyMgr =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        // Builds the notification and issues it.
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());
-
-        // Terminate Service after doing the necessary tasks
-        stopSelf();
-    }
+    // Terminate Service after doing the necessary tasks
+    stopSelf();
+  }
 }
--- app/src/main/java/com/example/v_mahich/client/ClientActivity.java
+++ app/src/main/java/com/example/v_mahich/client/ClientActivity.java
@@ -16,142 +16,151 @@ import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
 
-/* A sample Activity illustrating how to request for more files/feeds
+/**
+ * A sample Activity illustrating how to request for more files/feeds
  * to the Sneakernet Service
- *  */
+ */
 public class ClientActivity extends AppCompatActivity {
 
-    public static final String TAG = "ClientActivity";
+  public static final String TAG = "ClientActivity";
 
-    public static final int DONWLOAD_FEED = 1;
+  public static final int DONWLOAD_FEED = 1;
 
-    // The two kinds of requests we can send to sneakernet
-    public static final int SUBSCRIBE = 1;
-    public static final int UNSUBSCRIBE = 2;
+  // The two kinds of requests we can send to sneakernet
+  public static final int SUBSCRIBE = 1;
+  public static final int UNSUBSCRIBE = 2;
 
-    // The result returned from sneakernet for the request made
-    public static final int REQUEST_SUCCESSFUL = 1;
-    public static final int REQUEST_UNSUCCESSFUL = 2;
-    /**
-     * Target we publish to Sneakernet to communicate with the
-     * ClientActivity using the Incoming Handler     * .
-     */
-    final Messenger mMessenger = new Messenger(new IncomingHandler());
-    // Messenger object which allows IPC with the sneakernet service
-    Messenger mService = null;
-    // Indicator if the SneakernetService is still bound to the ClientActivity
-    boolean mBound;
-    // Button to Subscribe/Unsubscribe to feed
-    ToggleButton toggleButton;
-    private ServiceConnection mConnection = new ServiceConnection() {
-        public void onServiceConnected(ComponentName className, IBinder service) {
-            // This is called when the connection with the service has been
-            // established, giving us the object we can use to
-            // interact with the service.  We are communicating with the
-            // service using a Messenger, so here we get a client-side
-            // representation of that from the raw IBinder object.
-            Log.i(TAG, "onServiceConnected");
-            mService = new Messenger(service);
-            mBound = true;
-        }
+  // The result returned from sneakernet for the request made
+  public static final int REQUEST_SUCCESSFUL = 1;
+  public static final int REQUEST_UNSUCCESSFUL = 2;
 
-        public void onServiceDisconnected(ComponentName className) {
-            // This is called when the connection with the service has been
-            // unexpectedly disconnected -- that is, its process crashed.
-            mService = null;
-            mBound = false;
-        }
-    };
-
-    /* Method to send a Request to Sneakernet
-    * @param feedUrl Provide a unique, immutable name for this course
-    * @param path Where the files will be placed
-    * @param subscribeMode Whether to Subscribe/Unsubscribe to a feed
-    * @param callbackService A service to call when the download has been completed, to display
-    *                       notification and update database accordingly
-     */
-    public void sendRequestToSneakernet(String feedUrl, String path, int subscribeMode, String callbackService) {
-        if (!mBound) return;
-        Bundle b = new Bundle();
-        b.putInt("subscribeMode", subscribeMode);
-        b.putSerializable("feedUrl", feedUrl);
-        b.putSerializable("destinationPath", path);
-        b.putSerializable("callUponSuccess", callbackService);
-
-        // Create and send a message to the service, using a supported 'what' value
-        Message msg = Message.obtain(null, DONWLOAD_FEED, 0, 0);
-        // The Messenger object which allows the Sneakernet Service to communicate with the ClientActivity
-        msg.replyTo = mMessenger;
-        msg.setData(b);
-        try {
-            mService.send(msg);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+  /**
+   * Target we publish to Sneakernet to communicate with the
+   * ClientActivity using the Incoming Handler     * .
+   */
+  final Messenger mMessenger = new Messenger(new IncomingHandler());
+  // Messenger object which allows IPC with the sneakernet service
+  Messenger mService = null;
+  // Indicator if the SneakernetService is still bound to the ClientActivity
+  boolean mBound;
+  // Button to Subscribe/Unsubscribe to feed
+  ToggleButton toggleButton;
+  private ServiceConnection mConnection = new ServiceConnection() {
+    public void onServiceConnected(ComponentName className, IBinder service) {
+      // This is called when the connection with the service has been
+      // established, giving us the object we can use to
+      // interact with the service.  We are communicating with the
+      // service using a Messenger, so here we get a client-side
+      // representation of that from the raw IBinder object.
+      Log.i(TAG, "onServiceConnected");
+      mService = new Messenger(service);
+      mBound = true;
     }
+
+    public void onServiceDisconnected(ComponentName className) {
+      // This is called when the connection with the service has been
+      // unexpectedly disconnected -- that is, its process crashed.
+      mService = null;
+      mBound = false;
+    }
+  };
+
+  /**
+   * Method to send a Request to Sneakernet
+   *
+   * @param feedUrl Provide a unique, immutable name for this course
+   * @param path Where the files will be placed
+   * @param subscribeMode Whether to Subscribe/Unsubscribe to a feed
+   * @param callbackService A service to call when the download has been completed, to display
+   * notification and update database accordingly
+   */
+  public void sendRequestToSneakernet(String feedUrl, String path, int subscribeMode,
+      String callbackService) {
+    if (!mBound) {
+      return;
+    }
+    Bundle b = new Bundle();
+    b.putInt("subscribeMode", subscribeMode);
+    b.putSerializable("feedUrl", feedUrl);
+    b.putSerializable("destinationPath", path);
+    b.putSerializable("callUponSuccess", callbackService);
+
+    // Create and send a message to the service, using a supported 'what' value
+    Message msg = Message.obtain(null, DONWLOAD_FEED, 0, 0);
+    // The Messenger object which allows the Sneakernet Service to communicate with the ClientActivity
+    msg.replyTo = mMessenger;
+    msg.setData(b);
+    try {
+      mService.send(msg);
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    // Bind to the service
+    Intent i = new Intent();
+    i.setComponent(
+        new ComponentName("com.example.v_mahich.server", "com.example.v_mahich.server.MyService"));
+    bindService(i, mConnection,
+        Context.BIND_AUTO_CREATE);
+
+    toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if (!mBound) {
+          Log.w(TAG, "Not bound to Sneakernet Service");
+          return;
+        }
+        String feedUrl = "https://loremipsum.org";
+        String path = "/lorem/ipsum/video/course";
+        String callbackService = "com.example.v_mahich.client.CalbackService";
+        if (isChecked) {
+          sendRequestToSneakernet(feedUrl, path, SUBSCRIBE, callbackService);
+        } else {
+          sendRequestToSneakernet(feedUrl, path, UNSUBSCRIBE, callbackService);
+        }
+      }
+    });
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    // Unbind from the service
+    if (mBound) {
+      unbindService(mConnection);
+      mBound = false;
+    }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    toggleButton = findViewById(R.id.toggleButton);
+  }
+
+  /**
+   * Handler of incoming messages from SneakernetService.
+   * This returns the result of our request to subscribe/unsubscribe to a field.
+   */
+  class IncomingHandler extends Handler {
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        // Bind to the service
-        Intent i = new Intent();
-        i.setComponent(new ComponentName("com.example.v_mahich.server", "com.example.v_mahich.server.MyService"));
-        bindService(i, mConnection,
-                Context.BIND_AUTO_CREATE);
-
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (!mBound) {
-                    Log.w(TAG, "Not bound to Sneakernet Service");
-                    return;
-                }
-                String feedUrl = "https://loremipsum.org";
-                String path = "/lorem/ipsum/video/course";
-                String callbackService = "com.example.v_mahich.client.CalbackService";
-                if (isChecked) {
-                    sendRequestToSneakernet(feedUrl, path, SUBSCRIBE, callbackService);
-                } else {
-                    sendRequestToSneakernet(feedUrl, path, UNSUBSCRIBE, callbackService);
-                }
-            }
-        });
+    public void handleMessage(Message msg) {
+      switch (msg.what) {
+        case REQUEST_SUCCESSFUL:
+          Log.i(TAG, "Request completed Successfully.");
+          break;
+        case REQUEST_UNSUCCESSFUL:
+          Log.i(TAG, "Request failed.");
+          break;
+        default:
+          super.handleMessage(msg);
+      }
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // Unbind from the service
-        if (mBound) {
-            unbindService(mConnection);
-            mBound = false;
-        }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        toggleButton = findViewById(R.id.toggleButton);
-    }
-
-    /**
-     * Handler of incoming messages from SneakernetService.
-     * This returns the result of our request to subscribe/unsubscribe to a field.
-     */
-    class IncomingHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case REQUEST_SUCCESSFUL:
-                    Log.i(TAG, "Request completed Successfully.");
-                    break;
-                case REQUEST_UNSUCCESSFUL:
-                    Log.i(TAG, "Request failed.");
-                    break;
-                default:
-                    super.handleMessage(msg);
-            }
-        }
-    }
+  }
 }
